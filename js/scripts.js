2fe0c69b722e3b200238b6d7b88c9d89ac3ba4ca
Carlos Urquiza and Scott Scholz <scott@>
2016-06-20 19:33:38 -0700

    first working leap year detector.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 //business logic
+
 var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
@@ -10,13 +9,22 @@ var leapYear = function(year) {
 };
 
 
-
 // front end
+
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year =parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+      if (!result) {
+        $(".not").text("not");
+      } else {
+        $(".not").text("");
+      }
+
+      $("#result").show();
   });
 });
