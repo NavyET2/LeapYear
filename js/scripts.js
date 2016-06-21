34e232886621dacd9f575c2f2fe8ed7a94263bc2
Carlos Urquiza and Scott Scholz <carlos@>
2016-06-20 19:01:24 -0700

    second working parameter of jscript in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 //business logic
 var leapYear = function(year) {
-  if (year % 4 === 0) {
-  return true;
-} else {
-  return false;
-}
+  if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 
